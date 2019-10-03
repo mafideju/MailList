528d7b350017fb07ea24500fa5f872f1aa3d0c48
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2019-10-03 17:41:42 -0300

    feat(views => server): add view with form and post request

--- server.js
+++ server.js
@@ -1,30 +1,57 @@
-/* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 const express = require('express');
 const chalk = require('chalk');
-const faker = require('faker');
+// const faker = require('faker');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const server = express();
 const PORT = 7070;
-mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'maillist_db',
 });
 
-const tableData = [];
-for (let i = 0; i < 25; i++) {
-  tableData.push([
-    `${faker.name.firstName()} ${faker.name.lastName()}`,
-    faker.internet.email(),
-    faker.address.streetAddress(),
-    faker.address.city(),
-    faker.address.country(),
-    faker.date.past(),
-  ]);
-}
+server.set('view engine', 'ejs');
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.get('/', (req, res) => {
+  connection.query('SELECT COUNT(*) AS count FROM users', (err, data) => {
+    if (err) throw err;
+    // res.send(`Junte se à ${data[0].count} pessoas e receba nossa MafiMail`);
+    res.render('index', {
+      count: data[0].count,
+    });
+  });
+});
+
+server.post('/register', (req, res) => {
+  const person = {
+    email: req.body.email,
+  };
+  connection.query('INSERT INTO users SET ?', person, (err, data) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`\n Servidor Ativo: ${chalk.bgGreen(`Porta ${PORT}`)}\n`);
+});
+
+// const tableData = [];
+// for (let i = 0; i < 100; i++) {
+//   tableData.push([
+//     `${faker.name.firstName()} ${faker.name.lastName()}`,
+//     faker.internet.email(),
+//     faker.address.streetAddress(),
+//     faker.address.city(),
+//     faker.address.country(),
+//     faker.date.past(),
+//   ]);
+// }
 
 // connection.query('INSERT INTO users (name, email, street, city, country, member_since) VALUES ?',
 //   [tableData], (err, data) => {
@@ -33,16 +60,12 @@ for (let i = 0; i < 25; i++) {
 //   });
 // connection.end();
 
-server.get('/', (req, res) => {
-  res.send(`<div style="font-family:monospace;">
-    <h2>${faker.name.firstName()} ${faker.name.lastName()}</h2>
-    <h3>${faker.internet.email()}</h3>
-    <h3>${faker.address.streetName()}, ${Math.round(faker.random.number() / 100)}</h3>
-    <h4>${faker.address.city()} - ${faker.address.country()}</h4>
-    <h5>${faker.date.past()}</h5>
-    </div>`);
-});
-
-server.listen(PORT, () => {
-  console.log(`\n Servidor Ativo: ${chalk.bgGreen(`Porta ${PORT}`)}\n`);
-});
+// server.get('/', (req, res) => {
+//   res.send(`<div style="font-family:monospace;">
+//     <h2>${faker.name.firstName()} ${faker.name.lastName()}</h2>
+//     <h3>${faker.internet.email()}</h3>
+//     <h3>${faker.address.streetName()}, ${Math.round(faker.random.number() / 100)}</h3>
+//     <h4>${faker.address.city()} - ${faker.address.country()}</h4>
+//     <h5>${faker.date.past()}</h5>
+//     </div>`);
+// });
